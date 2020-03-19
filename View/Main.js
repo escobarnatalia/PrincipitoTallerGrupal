f2f793fff2ae586e9c55739434ee2800633ce537
Miguel Angel Rivera <angel1301999@gmail.com>
2020-03-19 11:56:05 -0500

    ingreso de imagenes y linkeo de clases en Main

---
+++ View/Main.js
@@ -0,0 +1,24 @@
+let logica;
+let cuento;
+let farolero;
+let personaje;
+let dia;
+let noche;
+let pantalla;
+
+function setup() {
+    createCanvas(880, 600);
+    logica = new Logica();
+    cuento = new CuentoView();
+    farolero = new FaroleroView();
+    personaje = new PersonajeView();
+    dia = new DiaView();
+    noche = new NocheView();4
+    pantalla = new PantallaView
+}
+function draw() {
+
+}
+function preload() {
+
+}

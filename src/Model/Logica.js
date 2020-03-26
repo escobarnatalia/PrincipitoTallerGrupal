c993c811a141957199b5c2d4a7ceca637b558d58
Miguel Angel Rivera <angel1301999@gmail.com>
2020-03-26 12:08:31 -0500

    generacion de array a partir del txt

--- src/Model/Logica.js
+++ src/Model/Logica.js
@@ -1,32 +1,68 @@
-let farolero,principe,planeta;
+let farolero, principe, planeta, cuento;
+let validarFarolero, validarPrincipito, validarFarol;
+let validarPlaneta;
 
-class Logica{
-  
-    constructor(){
-        
-        this.farolero = new Farolero(0,0);
-        this.principe = new Principe(0,0);
+class Logica {
+
+    constructor() {
+
+        this.validarPlaneta = false;
+        this.validarFarolero = false;
+        this.validarPrincipito = false;
+        this.validarFarol = false;
+        this.farolero = new Farolero(0, 0);
+        this.principe = new Principe(0, 0);
         this.planeta = new Planeta();
-        this.farol= new Farol();
+        this.farol = new Farol();
+        String cuento[] = loadStrings("./../cuento.txt");
 
 
     }
 
+    lecturaCuento() {
+
+        for (i = 0; i < cuento.length; i++) {
+            String guiaCuento[] = cuento[i].split(" ");
+
+            if (guiaCuento[0].equals("farolero")) {
+                this.validarFarolero = true;
+            }
+
+            if (guiaCuento[0].equals("principito")) {
+                this.validarPrincipito = true;
+            }
+
+            if (guiaCuento[0].equals("farol")) {
+                this.validarFarol = true;
+            }
+
+            if (guiaCuento[0].equals("planeta")) {
+                this.validarPlaneta = true;
+            }
+        }
+    }
 
 
-    pintarEspacio(){
-        this.planeta.pintarPlaneta();
-        
-      
-     }
+    pintarEspacio() {
 
-    pintarPersonajes(){
-        this.farol.pintarFarol();
-        this.farolero.pintarFarolero();
-        this.principe.pintarPrincipe();
-        
+        if (this.validarPlaneta = true) {
+            this.planeta.pintarPlaneta();
+        }
 
-     }
+    }
+
+    pintarPersonajes() {
+        if (this.validarFarol == true) {
+            this.farol.pintarFarol();
+        }
+        if (this.validarFarolero == true) {
+            this.farolero.pintarFarolero();
+        }
+        if (this.validarPrincipito == true) {
+            this.principe.pintarPrincipe();
+        }
+
+    }
 
 
 }
